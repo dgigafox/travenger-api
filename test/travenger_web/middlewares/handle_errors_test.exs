d3121924f66b6eab84080dfedc9d3fac6bca1e8c
Darren Gegantino <darrengegantino@gmail.com>
2019-02-05 18:23:04 +0800

    ✅ Add valdation test in error handler

--- test/travenger_web/middlewares/handle_errors_test.exs
+++ test/travenger_web/middlewares/handle_errors_test.exs
@@ -5,20 +5,23 @@ defmodule TravengerWeb.Middlewares.HandleErrorsTest do
 
   alias TravengerWeb.Middlewares.HandleErrors
 
-  @unique_contraint_msg "belongs to pair"
+  @unique_contraint_msg "already belongs to a group"
   @invalid_msg "is invalid"
+  @invalid_length_msg "should be 3 character(s)"
 
   describe "Handle Error Call receiving Absinthe resolution with changeset error" do
     test "parses and returns changeset errors as list of maps grouped by keys" do
       ch =
-        {%{}, %{}}
-        |> change()
+        {%{}, %{name: :string}}
+        |> cast(%{name: "test"}, [:name])
+        |> validate_length(:name, is: 3)
         |> add_error(:symbol, @invalid_msg)
         |> add_error(:symbol, @unique_contraint_msg)
         |> add_error(:type, @invalid_msg)
 
       expected = %{
         errors: [
+          %{key: :name, message: [@invalid_length_msg]},
           %{key: :symbol, message: [@unique_contraint_msg, @invalid_msg]},
           %{key: :type, message: [@invalid_msg]}
         ]
