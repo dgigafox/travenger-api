4884848aea750b1c408331fc5b09668543cce3ef
Darren Gegantino <darren.gegantino@martide.com>
2019-09-26 01:27:51 +0800

    Remove anything related to default page controller

--- test/travenger_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule TravengerWeb.PageControllerTest do
-  use TravengerWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
